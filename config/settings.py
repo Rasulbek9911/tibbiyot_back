458a096b8b4cccc2678eb9828023dbf0e9c40e5a
Rasulbek9911 <rasulbek1301@gmail.com>
2023-11-16 12:19:40 +0500

    translation added

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!94x9)kqgus51e&e!_ihv-)z^9tvwpr7=zr6r8z^nw0-(n@sy_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
