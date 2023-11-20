b9a201c885a2e8b7d6cbeb22c3a13160a0d1b7fc
Rasulbek9911 <rasulbek1301@gmail.com>
2023-11-20 11:50:55 +0500

    test blck

--- home_app/views.py
+++ home_app/views.py
@@ -1,22 +1,19 @@
 from rest_framework.generics import ListAPIView
-from .serializers import (
-    CommonInformationSerializer,
-    MeyoriyHujjatlarSerializer,
-    RahbariyatSerializer,
-    SsbBuyruqSerializer,
-)
+from .serializers import CommonInformationSerializer,MeyoriyHujjatlarSerializer,RahbariyatSerializer,SsbBuyruqSerializer,
 from .models import CommonInformation, MeyoriyHujjatlar, Rahbariyat, SsbBuyruq
 from .pagination import DoubleShort
-
-
 class CommonInformationView(ListAPIView):
     serializer_class = CommonInformationSerializer
     queryset = CommonInformation.objects.all()
-
-
 class MeyoriyHujjatlarView(ListAPIView):
     serializer_class = MeyoriyHujjatlarSerializer
     pagination_class = DoubleShort
+
+
+
+
+
+
     queryset = MeyoriyHujjatlar.objects.all()
 
 
@@ -25,7 +22,17 @@ class RahbariyatView(ListAPIView):
     queryset = Rahbariyat.objects.all()
 
 
+
+
+
+
+
 class SsbBuyruqView(ListAPIView):
     serializer_class = SsbBuyruqSerializer
     pagination_class = DoubleShort
+
+
+
+
+
     queryset = SsbBuyruq.objects.all()
