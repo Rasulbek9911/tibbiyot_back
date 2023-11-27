a76feb298cac2ffe364295660993775ab5d692c3
shohbek <jalilovshohbek9909@gmail.com>
2023-11-27 15:03:17 +0500

    add test file

--- tibbiyot_front/src/test/Test.jsx
+++ tibbiyot_front/src/test/Test.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Test() {
   return (
     <div>
-      
+      <h1>test</h1>
     </div>
-  )
+  );
 }
 
-export default Test
+export default Test;
