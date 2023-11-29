73c8469ed988cf7474d6d4c4132cce581c04c4b6
shohbek <jalilovshohbek9909@gmail.com>
2023-11-29 12:46:28 +0500

    add pdf file

---
+++ tibbiyot_front/src/pages/home/News.jsx
@@ -0,0 +1,203 @@
+import React from "react";
+import image from "../../assets/images/medic.png";
+
+function News() {
+  return (
+    <div className="news">
+      <div className="rows">
+        <div className="cardss">
+          <div className="imgBlok">
+            <img src={image} alt="" />
+          </div>
+          <div className="desc">
+            <div className="date">
+              <span>
+                {" "}
+                <i
+                  className="fa fa-calendar-check-o"
+                  aria-hidden="true"
+                ></i>{" "}
+                18:00
+              </span>
+              <span>20.11.2023</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus tempora, deserunt nihil voluptatibus vitae delectus
+              odio!
+            </p>
+          </div>
+        </div>
+        <div className="cardss">
+          <div className="imgBlok">
+            <img src={image} alt="" />
+          </div>
+
+          <div className="desc">
+            <div className="date">
+              <span>
+                {" "}
+                <i
+                  className="fa fa-calendar-check-o"
+                  aria-hidden="true"
+                ></i>{" "}
+                18:00
+              </span>
+              <span>20.11.2023</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus tempora, deserunt nihil voluptatibus vitae delectus
+              odio!
+            </p>
+          </div>
+        </div>
+        <div className="cardss">
+          <div className="imgBlok">
+            <img src={image} alt="" />
+          </div>
+
+          <div className="desc">
+            <div className="date">
+              <span>
+                {" "}
+                <i
+                  className="fa fa-calendar-check-o"
+                  aria-hidden="true"
+                ></i>{" "}
+                18:00
+              </span>
+              <span>20.11.2023</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus tempora, deserunt nihil voluptatibus vitae delectus
+              odio!
+            </p>
+          </div>
+        </div>
+        <div className="cardss">
+          <div className="imgBlok">
+            <img src={image} alt="" />
+          </div>
+
+          <div className="desc">
+            <div className="date">
+              <span>
+                {" "}
+                <i
+                  className="fa fa-calendar-check-o"
+                  aria-hidden="true"
+                ></i>{" "}
+                18:00
+              </span>
+              <span>20.11.2023</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus tempora, deserunt nihil voluptatibus vitae delectus
+              odio!
+            </p>
+          </div>
+        </div>
+        <div className="cardss">
+          <div className="imgBlok">
+            <img src={image} alt="" />
+          </div>
+          <div className="desc">
+            <div className="date">
+              <span>
+                {" "}
+                <i
+                  className="fa fa-calendar-check-o"
+                  aria-hidden="true"
+                ></i>{" "}
+                18:00
+              </span>
+              <span>20.11.2023</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus tempora, deserunt nihil voluptatibus vitae delectus
+              odio!
+            </p>
+          </div>
+        </div>
+        <div className="cardss">
+          <div className="imgBlok">
+            <img src={image} alt="" />
+          </div>
+
+          <div className="desc">
+            <div className="date">
+              <span>
+                {" "}
+                <i
+                  className="fa fa-calendar-check-o"
+                  aria-hidden="true"
+                ></i>{" "}
+                18:00
+              </span>
+              <span>20.11.2023</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus tempora, deserunt nihil voluptatibus vitae delectus
+              odio!
+            </p>
+          </div>
+        </div>
+        <div className="cardss">
+          <div className="imgBlok">
+            <img src={image} alt="" />
+          </div>
+
+          <div className="desc">
+            <div className="date">
+              <span>
+                {" "}
+                <i
+                  className="fa fa-calendar-check-o"
+                  aria-hidden="true"
+                ></i>{" "}
+                18:00
+              </span>
+              <span>20.11.2023</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus tempora, deserunt nihil voluptatibus vitae delectus
+              odio!
+            </p>
+          </div>
+        </div>
+        <div className="cardss">
+          <div className="imgBlok">
+            <img src={image} alt="" />
+          </div>
+
+          <div className="desc">
+            <div className="date">
+              <span>
+                {" "}
+                <i
+                  className="fa fa-calendar-check-o"
+                  aria-hidden="true"
+                ></i>{" "}
+                18:00
+              </span>
+              <span>20.11.2023</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus tempora, deserunt nihil voluptatibus vitae delectus
+              odio!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default News;
