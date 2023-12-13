8b6c175b0506fc5bdf2ed4dd56cca8cc601a9530
shohbek <jalilovshohbek9909@gmail.com>
2023-12-13 18:15:26 +0500

    remove Pullik resurslar

--- tibbiyot_front/src/pages/fanlarBoyichaOquvResurslariAll/PullikResurslar.jsx
+++ tibbiyot_front/src/pages/fanlarBoyichaOquvResurslariAll/PullikResurslar.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 function PullikResurslar() {
-  return (
-    <>
-      <Outlet />
-    </>
-  );
+  return <>{/* <Outlet /> */}</>;
 }
 
 export default PullikResurslar;
