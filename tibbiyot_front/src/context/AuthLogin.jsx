cdab541a95a27bdf3e113a0e8a0e093b17582cde
shohbek <jalilovshohbek9909@gmail.com>
2023-12-19 14:34:22 +0500

    fix

--- tibbiyot_front/src/context/AuthLogin.jsx
+++ tibbiyot_front/src/context/AuthLogin.jsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react";
-import { createContext } from "react";
+// import { useEffect, useState } from "react";
+// import { createContext } from "react";
 
-export const LoginContext = createContext();
+// export const LoginContext = createContext();
 
-export const LoginProvider = ({ children }) => {
-  const localAccessToken = localStorage.getItem("AccessToken");
-  const localRefreshToken = localStorage.getItem("RefreshToken");
-  const [accessToken, SetAccessToken] = useState(JSON.parse(localAccessToken));
-  const [refreshToken, SetRefreshToken] = useState(
-    JSON.parse(localRefreshToken)
-  );
-  useEffect(() => {
-    localStorage.setItem("AccessToken", JSON.stringify(accessToken));
-  }, [accessToken]);
+// export const LoginProvider = ({ children }) => {
+//   const localAccessToken = localStorage.getItem("AccessToken");
+//   const localRefreshToken = localStorage.getItem("RefreshToken");
+//   const [accessToken, SetAccessToken] = useState(JSON.parse(localAccessToken));
+//   const [refreshToken, SetRefreshToken] = useState(
+//     JSON.parse(localRefreshToken)
+//   );
+//   useEffect(() => {
+//     localStorage.setItem("AccessToken", JSON.stringify(accessToken));
+//   }, [accessToken]);
 
-  useEffect(() => {
-    localStorage.setItem("RefreshToken", JSON.stringify(refreshToken));
-  }, [refreshToken]);
-  return (
-    <LoginContext.Provider
-      value={{ accessToken, SetAccessToken, refreshToken, SetRefreshToken }}
-    >
-      {children}
-    </LoginContext.Provider>
-  );
-};
+//   useEffect(() => {
+//     localStorage.setItem("RefreshToken", JSON.stringify(refreshToken));
+//   }, [refreshToken]);
+//   return (
+//     <LoginContext.Provider
+//       value={{ accessToken, SetAccessToken, refreshToken, SetRefreshToken }}
+//     >
+//       {children}
+//     </LoginContext.Provider>
+//   );
+// };
