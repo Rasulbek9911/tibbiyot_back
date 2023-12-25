2e331eb8c593f94885e1de763bd461c149583237
shohbek <jalilovshohbek9909@gmail.com>
2023-12-25 19:09:31 +0500

    add foydali malumot modal

--- tibbiyot_front/src/pages/umumiyMalumot/components/OqXalatModal.jsx
+++ tibbiyot_front/src/pages/umumiyMalumot/components/OqXalatModal.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./oqXalatModal.scss";
+import { useNavigate, useParams } from "react-router-dom";
+import { useGetFetch } from "../../../hooks/useGetFetch";
+import { baseUrl } from "../../../services/http";
 
 function OqXalatModal() {
+  const { javob } = useParams();
+  const { data } = useGetFetch(`${baseUrl}/foydali_malumot/${javob}`);
+  const navigate = useNavigate();
+  if (!data) {
+    return <p></p>;
+  }
   return (
     <div className="javobContent">
       <div className="javobModal">
-        <i className="fa fa-times closeBtn" aria-hidden="true"></i>
-        {/* <p dangerouslySetInnerHTML={{ __html: data?.description }}></p> */}
+        <i
+          className="fa fa-times closeBtn"
+          aria-hidden="true"
+          onClick={() => navigate(-1)}
+        ></i>
+        <h2>{data.title}</h2>
+        <p dangerouslySetInnerHTML={{ __html: data?.description }}></p>
       </div>
     </div>
   );
